a09614d6fbc3bafe26852fffec8e4a7ca6d54924
Alam <aalam23@CougarNet.UH.EDU>
2026-01-15 20:40:59 -0600

    CORS UPDATE

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,9 @@ app.use(express.static(path));
 
 const corsOptions = {
   origin: [
-    "http://localhost:4200"
+    "http://localhost:4200",
+    "http://localhost:8080",
+    "http://equipment.local"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
